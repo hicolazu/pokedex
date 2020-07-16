e8fb3c04ab5a50a9ad5a0459c34cecab9db7fda8
Henrico <hicolazu22@hotmail.com>
2020-07-15 22:43:20 -0300

    feat: add mongodb configuration

--- src/main/java/br/com/henricolazuroz/pokedex/PokedexApplication.java
+++ src/main/java/br/com/henricolazuroz/pokedex/PokedexApplication.java
@@ -1,7 +1,13 @@
 package br.com.henricolazuroz.pokedex;
 
+import br.com.henricolazuroz.pokedex.model.Pokemon;
+import br.com.henricolazuroz.pokedex.repository.PokedexRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.core.ReactiveMongoOperations;
+import reactor.core.publisher.Flux;
 
 @SpringBootApplication
 public class PokedexApplication {
@@ -10,4 +16,20 @@ public class PokedexApplication {
 		SpringApplication.run(PokedexApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner init (ReactiveMongoOperations operations,
+							PokedexRepository repository) {
+		return args -> {
+			Flux<Pokemon> pokedexFlux = Flux.just(
+					new Pokemon(null, "Bulbassauro", "Semente", "OverGrow", 6.09),
+					new Pokemon(null, "Charizard", "Fogo", "Blaze", 90.05),
+					new Pokemon(null, "Caterpie", "Minhoca", "Poeira do Escudo", 2.09),
+					new Pokemon(null, "Blastoise", "Marisco", "Torrente", 6.09))
+					.flatMap(repository::save);
+
+			pokedexFlux
+					.thenMany(repository.findAll())
+					.subscribe(System.out::println);
+		};
+	}
 }
